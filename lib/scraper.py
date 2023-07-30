a0aed0ac0dae7485c180a5ded0f998a2e1aa3fab
matthew rendely <matthew.rendely@gmail.com>
2023-07-30 10:15:34 -0700

    Done

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,6 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')
+courses = doc.select('.heading-25-extrabold')
+[print(c.contents[0].strip()) for c in courses]
